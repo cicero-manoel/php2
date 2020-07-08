bbd54cfa23d78c0b943111759e9ea4f4b20f3859
cicero manoel <ciceromanoel2323@gmail.com>
2020-07-08 15:10:05 -0300

    Primeiro Commit e mais alguns arquivos enserido

---
+++ README.md
@@ -0,0 +1,1 @@
+Iniciando Sistema GIT
---
+++ adicionar.php
@@ -0,0 +1,17 @@
+<h1>ADICIONAR USUARIO</h1>
+
+<form method="POST" action="adicionar_action.php">
+    <label>
+        NOME:<br/>
+        <input type="text" name="name" />
+    </label><br/><br/>
+
+    <label>
+        E-MAIL:<br/>
+        <input type="email" name="email" />
+    </label><br/><br/>
+
+ 
+    <input type="submit" value="adicionar" />
+   
+</form>
